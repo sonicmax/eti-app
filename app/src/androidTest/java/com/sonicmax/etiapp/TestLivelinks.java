79d144e36af336abec796c5fcce237bf6a34a1b9
max <max.teh.lueser@gmail.com>
2015-12-07 23:27:55 +0000

    Add minor changes to livelinks test

--- app/src/androidTest/java/com/sonicmax/etiapp/TestLivelinks.java
+++ app/src/androidTest/java/com/sonicmax/etiapp/TestLivelinks.java
@@ -8,29 +8,34 @@ public class TestLivelinks extends AndroidTestCase {
     final String LOG_TAG = TestLivelinks.class.getSimpleName();
     // Use your own topic/user ID for following variables. Get payload values by checking livelinks
     // request payload for given topic/user ID in your browser.
-    final int topicId = 9299399;
-    final int userId = 5599;
-    final long payloadTopic = 144115188085155271L;
-    final long payloadUser = 72057594037933535L;
+    final static int topicId = 9299399;
+    final static int userId = 5599;
+    final static long payloadTopic = 144115188085155271L;
+    final static long payloadInbox = 72057594037933535L;
+    final int topicChannel = 0x100;
+    final int inboxChannel = 0x200;
 
     public void testIdGeneration() {
-        // Zero out top 8 bits to get topic/user id from long
-        long topicFromPayload = payloadTopic & 0x00FFFFFFFFFFFFFFL;
-        long userFromPayload = payloadUser & 0x00FFFFFFFFFFFFFFL;
+        long testPayload;
 
-        if (BuildConfig.DEBUG && !(topicFromPayload == topicId)) {
-            Log.v(LOG_TAG, "Expected " + topicId + ", got " + topicFromPayload);
+        // Zero out top 8 bits to get topic id from long
+        testPayload = payloadTopic & 0x00ffffffL;
+
+        if (BuildConfig.DEBUG && !(testPayload == topicId)) {
+            Log.v(LOG_TAG, "Expected " + topicId + ", got " + testPayload);
             throw new AssertionError();
         } else {
-            Log.v(LOG_TAG, "Topic ID: " + topicFromPayload);
+            Log.v(LOG_TAG, "Topic ID: " + testPayload);
         }
 
-        if (BuildConfig.DEBUG && !(userFromPayload == userId)) {
-            Log.v(LOG_TAG, "Expected " + userId + ", got " + userFromPayload);
+        // Zero out top 8 bits to get user id from long
+        testPayload = payloadInbox & 0x00ffffffL;
+
+        if (BuildConfig.DEBUG && !(testPayload == userId)) {
+            Log.v(LOG_TAG, "Expected " + userId + ", got " + testPayload);
             throw new AssertionError();
         } else {
-            Log.v(LOG_TAG, "User ID: " + userFromPayload);
+            Log.v(LOG_TAG, "User ID: " + testPayload);
         }
     }
-
 }
