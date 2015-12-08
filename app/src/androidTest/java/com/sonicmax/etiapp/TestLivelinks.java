f180fe412de50a4aedca640d7cba712119279d0e
max <max.teh.lueser@gmail.com>
2015-12-08 02:54:59 +0000

    Implement payload generation in LivelinksHandler, update test case

--- app/src/androidTest/java/com/sonicmax/etiapp/TestLivelinks.java
+++ app/src/androidTest/java/com/sonicmax/etiapp/TestLivelinks.java
@@ -3,6 +3,8 @@ package com.sonicmax.etiapp;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
+import com.sonicmax.etiapp.network.LivelinksHandler;
+
 import java.math.BigInteger;
 
 public class TestLivelinks extends AndroidTestCase {
@@ -13,36 +15,40 @@ public class TestLivelinks extends AndroidTestCase {
     final static long PAYLOAD_TOPIC_KEY = 144115188085085408L;
     final static long PAYLOAD_INBOX_KEY = 72057594037933535L;
 
+    private final LivelinksHandler mLivelinks;
+
+    public TestLivelinks() {
+        this.mLivelinks = new LivelinksHandler(getContext(), TOPIC_ID, USER_ID);
+    }
+
     public void testPayloadGeneration() {
         final String LOG_TAG = "testPayloadGeneration";
-        final int TOPIC_CHANNEL = 0x0200;
-        final int INBOX_CHANNEL = 0x0100;
-        final int SHIFT_CONSTANT = 48;
 
-        // Payload consists of channel type and identifier packed into 64-bit int.
-        BigInteger topicForPayload = BigInteger.valueOf(TOPIC_CHANNEL)
-                // 100000000
-                .shiftLeft(SHIFT_CONSTANT) // SHIFT_CONSTANT is 48
-                // 1000000000000000000000000000000000000000000000000000000000
-                .or(BigInteger.valueOf(TOPIC_ID));
-                // 1000000000000000000000000000000000100011011110010111000111 == 144115188085085408
+        // BigInteger payload = BigInteger.valueOf(CHANNEL) // 100000000
 
-        if (BuildConfig.DEBUG && !(topicForPayload.equals(BigInteger.valueOf(PAYLOAD_TOPIC_KEY)))) {
+                // .shiftLeft(SHIFT_CONSTANT) // SHIFT_CONSTANT is 48
+                //      1000000000000000000000000000000000000000000000000000000000
+
+                // .or(BigInteger.valueOf(ID));
+                //      1000000000000000000000000000000000100011011110010111000111
+
+        
+        BigInteger topicPayload = mLivelinks.getTopicPayload();
+        BigInteger inboxPayload = mLivelinks.getInboxPayload();
+
+        if (BuildConfig.DEBUG && !(topicPayload.equals(BigInteger.valueOf(PAYLOAD_TOPIC_KEY)))) {
 
             Log.d(LOG_TAG, "Expected: " + BigInteger.valueOf(PAYLOAD_TOPIC_KEY).toString(2)
-                    + "\n Generated: " + topicForPayload.toString(2));
+                    + "\n Generated: " + topicPayload.toString(2));
+
             throw new AssertionError();
 
         }
 
-        BigInteger inboxForPayload = BigInteger.valueOf(INBOX_CHANNEL)
-                .shiftLeft(SHIFT_CONSTANT)
-                .or(BigInteger.valueOf(USER_ID));
-
-        if (BuildConfig.DEBUG && !(inboxForPayload.equals(BigInteger.valueOf(PAYLOAD_INBOX_KEY)))) {
+        if (BuildConfig.DEBUG && !(inboxPayload.equals(BigInteger.valueOf(PAYLOAD_INBOX_KEY)))) {
 
             Log.d(LOG_TAG, "Expected: " + BigInteger.valueOf(PAYLOAD_INBOX_KEY).toString(2)
-                    + "\n Generated: " + inboxForPayload.toString(2));
+                    + "\n Generated: " + inboxPayload.toString(2));
 
             throw new AssertionError();
 
--- app/src/main/java/com/sonicmax/etiapp/network/LivelinksHandler.java
+++ app/src/main/java/com/sonicmax/etiapp/network/LivelinksHandler.java
@@ -1,12 +1,37 @@
 package com.sonicmax.etiapp.network;
 
-/**
- *
- */
+import android.content.Context;
+
+import java.math.BigInteger;
 
 public class LivelinksHandler {
 
+    final Context mContext;
+    final BigInteger topicPayload;
+    final BigInteger inboxPayload;
 
+    public LivelinksHandler(Context context, int topicId, int userId) {
+        final int TOPIC_CHANNEL = 0x0200;
+        final int INBOX_CHANNEL = 0x0100;
+        final int SHIFT_CONSTANT = 48;
 
+        this.mContext = context;
 
+        // Payload consists of channel type and identifier packed into 64-bit int.
+        topicPayload = BigInteger.valueOf(TOPIC_CHANNEL)
+                .shiftLeft(SHIFT_CONSTANT)
+                .or(BigInteger.valueOf(topicId));
+
+        inboxPayload = BigInteger.valueOf(INBOX_CHANNEL)
+                .shiftLeft(SHIFT_CONSTANT)
+                .or(BigInteger.valueOf(userId));
+    }
+
+    public BigInteger getTopicPayload() {
+        return topicPayload;
+    }
+
+    public BigInteger getInboxPayload() {
+        return inboxPayload;
+    }
 }
