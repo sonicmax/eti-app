4e53849b068a90428a26d92ee7d4548748b9bc8f
max <max.teh.lueser@gmail.com>
2015-12-08 02:18:05 +0000

    Complete livelinks payload test case

--- app/src/androidTest/java/com/sonicmax/etiapp/TestLivelinks.java
+++ app/src/androidTest/java/com/sonicmax/etiapp/TestLivelinks.java
@@ -3,39 +3,49 @@ package com.sonicmax.etiapp;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
-public class TestLivelinks extends AndroidTestCase {
+import java.math.BigInteger;
 
-    final String LOG_TAG = TestLivelinks.class.getSimpleName();
+public class TestLivelinks extends AndroidTestCase {
     // Use your own topic/user ID for following variables. Get payload values by checking livelinks
     // request payload for given topic/user ID in your browser.
-    final static int topicId = 9299399;
-    final static int userId = 5599;
-    final static long payloadTopic = 144115188085155271L;
-    final static long payloadInbox = 72057594037933535L;
-    final int topicChannel = 0x100;
-    final int inboxChannel = 0x200;
+    final static int TOPIC_ID = 9229536;
+    final static int USER_ID = 5599;
+    final static long PAYLOAD_TOPIC_KEY = 144115188085085408L;
+    final static long PAYLOAD_INBOX_KEY = 72057594037933535L;
 
-    public void testIdGeneration() {
-        long testPayload;
+    public void testPayloadGeneration() {
+        final String LOG_TAG = "testPayloadGeneration";
+        final int TOPIC_CHANNEL = 0x0200;
+        final int INBOX_CHANNEL = 0x0100;
+        final int SHIFT_CONSTANT = 48;
 
-        // Zero out top 8 bits to get topic id from long
-        testPayload = payloadTopic & 0x00ffffffL;
+        // Payload consists of channel type and identifier packed into 64-bit int.
+        BigInteger topicForPayload = BigInteger.valueOf(TOPIC_CHANNEL)
+                // 100000000
+                .shiftLeft(SHIFT_CONSTANT) // SHIFT_CONSTANT is 48
+                // 1000000000000000000000000000000000000000000000000000000000
+                .or(BigInteger.valueOf(TOPIC_ID));
+                // 1000000000000000000000000000000000100011011110010111000111 == 144115188085085408
 
-        if (BuildConfig.DEBUG && !(testPayload == topicId)) {
-            Log.v(LOG_TAG, "Expected " + topicId + ", got " + testPayload);
+        if (BuildConfig.DEBUG && !(topicForPayload.equals(BigInteger.valueOf(PAYLOAD_TOPIC_KEY)))) {
+
+            Log.d(LOG_TAG, "Expected: " + BigInteger.valueOf(PAYLOAD_TOPIC_KEY).toString(2)
+                    + "\n Generated: " + topicForPayload.toString(2));
             throw new AssertionError();
-        } else {
-            Log.v(LOG_TAG, "Topic ID: " + testPayload);
+
         }
 
-        // Zero out top 8 bits to get user id from long
-        testPayload = payloadInbox & 0x00ffffffL;
+        BigInteger inboxForPayload = BigInteger.valueOf(INBOX_CHANNEL)
+                .shiftLeft(SHIFT_CONSTANT)
+                .or(BigInteger.valueOf(USER_ID));
 
-        if (BuildConfig.DEBUG && !(testPayload == userId)) {
-            Log.v(LOG_TAG, "Expected " + userId + ", got " + testPayload);
+        if (BuildConfig.DEBUG && !(inboxForPayload.equals(BigInteger.valueOf(PAYLOAD_INBOX_KEY)))) {
+
+            Log.d(LOG_TAG, "Expected: " + BigInteger.valueOf(PAYLOAD_INBOX_KEY).toString(2)
+                    + "\n Generated: " + inboxForPayload.toString(2));
+
             throw new AssertionError();
-        } else {
-            Log.v(LOG_TAG, "User ID: " + testPayload);
+
         }
     }
 }
