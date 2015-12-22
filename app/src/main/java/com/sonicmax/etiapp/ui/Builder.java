ea310c93abe7374893ee04a9ae1b8913f7a35108
max <max.teh.lueser@gmail.com>
2015-12-22 21:42:37 +0000

    Clarify Builder documentation

--- app/src/main/java/com/sonicmax/etiapp/ui/Builder.java
+++ app/src/main/java/com/sonicmax/etiapp/ui/Builder.java
@@ -3,7 +3,10 @@ package com.sonicmax.etiapp.ui;
 import android.text.SpannableStringBuilder;
 
 /**
- * Class for MessageBuilder and SupportMessageBuilder to extend.
+ * Class for MessageBuilder and SupportMessageBuilder to extend. Makes MessageListAdapter simpler
+ * as we can check API level in constructor and assign either type of builder to same variable,
+ * instead of having to check whether we have used MessageBuilder/SupportMessageBuilder each time
+ * we want to update the UI.
  */
 public class Builder {
 
