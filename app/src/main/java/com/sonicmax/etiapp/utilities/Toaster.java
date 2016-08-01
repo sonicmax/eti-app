7211c1eb64b8129c4a17a261772284e69b2df871
max <max.teh.lueser@gmail.com>
2016-08-01 20:50:42 +0100

    Clean up Toaster class
    Don't need to check SDK version anymore as minSdkVersion is always  > 11 (minimum version for Toast)

--- app/src/main/java/com/sonicmax/etiapp/utilities/Toaster.java
+++ app/src/main/java/com/sonicmax/etiapp/utilities/Toaster.java
@@ -1,25 +1,18 @@
 package com.sonicmax.etiapp.utilities;
 
 import android.content.Context;
-import android.os.Build;
 import android.widget.Toast;
 
 public class Toaster {
-
     private static Toast mToast;
 
     public static void makeToast(Context context, String text) {
-
-        if (mToast != null)
+        if (mToast != null) {
             mToast.cancel();
-
-        boolean condition = Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB;
-
-        if ((mToast == null && condition) || !condition) {
-            mToast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
-        }
-        else if ((mToast != null && condition)) {
             mToast.setText(text);
+
+        } else {
+            mToast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
         }
 
         mToast.show();
